a2aa429aeb86f7bf75bd05532c7074e22302c05b
Alapponese <60528214+Alapponese@users.noreply.github.com>
2021-08-04 06:59:53 -0400

    Update Project 1.py
    Swapped out initial high score for a string. Then to handle the error of trying to find the greater than value, I added a try exception to the bottom. I also added "yes/no" prompts to questions.

--- Project 1.py
+++ Project 1.py
@@ -4,20 +4,21 @@ import random
 # Introduction
 print("------ Welcome to Angelo's Super Special Number Guessing Game! ------")
 user_name = input("What is your name?  ")
+print("The rules are simple... guess the number I am thinking of...")
 
 # This part gave me the most trouble. I didn't know how to get this variable to be used inside the function.
 # I kept getting an error that said "referenced before assignment". I googled it and found that i need to
 # label this a global variable inside my function. The website I got this answer from is:
 # https://stackoverflow.com/questions/855493/referenced-before-assignment-error-in-python
 
-highscore = int(9999999)
+highscore = ("not yet set")
 
 def start_game():
     num_of_guesses = 1
     win_num = random.randint(1,10)
     num_guess = int(987654321)
     global highscore
-    print("\nOkay {},".format(user_name))
+    print("\nOkay {}, the high score is {}.\nLet's see what you got!\n".format(user_name, highscore))
     while num_guess != win_num:
         try:
         # The part that kept messing me up where to put the user input line.
@@ -38,10 +39,14 @@ def start_game():
     else:
         print("""\nCongrats {}! The number was {}!
 You guessed the correct answer and it only took {} tries!\n""".format(user_name, win_num, num_of_guesses))
-        if num_of_guesses < highscore:
+        try:
+            if num_of_guesses < highscore:
+                highscore = num_of_guesses
+            else:
+                print("Sorry but you did not break the highscore of {}...\n".format(highscore))
+        except TypeError:
             highscore = num_of_guesses
-        else:
-            print("Sorry but you did not break the highscore of {}...\n".format(highscore))
+            print("The new highscore is {}!\n".format(highscore))
 
 start_game()
 replay = "yes"
